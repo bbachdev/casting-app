168e8cc6009c10bb95e48f9d2be48e2f9a629c20
Barry Bach <bbach7139@gmail.com>
2024-07-30 12:12:44 -0500

    Create basic layout for main pages

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className={`mt-auto w-full flex items-center py-4 px-6 bg-slate-900 text-white shadow-md dark:bg-slate-500`}>
+      <p className={`w-full text-center`}>Footer</p>
+    </footer>
+  )
+}
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className={`w-full flex py-4 px-6 bg-sky-900 text-white shadow-md dark:bg-sky-500`}>
+      Header
+    </header>
+  )
+}
