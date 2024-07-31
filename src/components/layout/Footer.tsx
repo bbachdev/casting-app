f5057068e9830692629c3807148fb8b62df8b07a
Barry Bach <bbach7139@gmail.com>
2024-07-31 12:21:32 -0500

    Adjust layout width for larger screen sizes

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,10 @@
 export default function Footer() {
   return (
-    <footer className={`mt-auto w-full flex items-center py-4 px-6 bg-slate-500 text-white shadow-md dark:bg-slate-500`}>
-      <p className={`w-full text-center`}>Footer</p>
+    <footer className={`mt-auto w-full flex py-4 px-6 bg-sky-950 text-white shadow-md`}>
+      <div className={`max-w-[1440px] w-full mx-auto`}>
+        <p className={`text-center`}>Footer</p>
+      </div>
+      
     </footer>
   )
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Button } from '../ui/button';
 import { useTheme } from "next-themes"
+import Link from 'next/link';
 import { MdLightMode } from "react-icons/md";
 import { MdDarkMode } from "react-icons/md";
 
@@ -13,16 +14,21 @@ export default function Header() {
   }
 
   return (
-    <header className={`w-full flex py-4 px-6 bg-sky-900 shadow-md dark:bg-sky-500`}>
-      {/* <header className={`w-full flex flex-row py-4 px-6 bg-transparent text-white`}> */}
-      <ul className={`ml-auto flex justify-center gap-4`}>
-        <Button variant="outline">
-          Sign In
-        </Button>
-        <Button variant="outline" onClick={() => toggleTheme()}>
-          {resolvedTheme === "dark" ? <MdLightMode /> : <MdDarkMode />}
-        </Button>
-      </ul>
+    <header className={`w-full flex py-4 px-6 bg-sky-900 shadow-md`}>
+      <div className={`flex max-w-[1440px] w-full mx-auto`}>
+        <ul className={`flex my-auto text-white`}>
+          <Link href="/">Home</Link>
+        </ul>
+        <ul className={`ml-auto flex my-auto gap-4`}>
+          <Button variant="outline">
+            Sign In
+          </Button>
+          <Button variant="outline" onClick={() => toggleTheme()}>
+            {resolvedTheme === "dark" ? <MdLightMode /> : <MdDarkMode />}
+          </Button>
+        </ul>
+      </div>
+      
       
     </header>
   )
