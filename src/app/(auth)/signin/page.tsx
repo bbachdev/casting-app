58fd8aa4f58584627b5583455bde1527f6c0a6bf
Barry Bach <bbach7139@gmail.com>
2024-08-01 13:52:50 -0500

    Add loader spinner library + update Sign In page

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,7 +1,37 @@
+'use client'
 import ContentContainer from '@/components/layout/ContentContainer';
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
+import { z } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { ThreeDots } from 'react-loader-spinner';
+import { useTransition } from 'react';
+
+const SignInSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(12),
+});
 
 export default function SignIn() {
+  const [isPending, startTransition] = useTransition()
+  
+  const form = useForm<z.infer<typeof SignInSchema>>({
+    resolver: zodResolver(SignInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof SignInSchema>) {
+    startTransition(() => {
+      console.log(values)
+    })
+  }
+
   return (
     <ContentContainer>
       <Card className={`mt-12 mx-auto flex flex-col items-center justify-center w-1/3`}>
@@ -9,7 +39,35 @@ export default function SignIn() {
           <h1 className={`text-3xl font-bold`}>Sign In</h1>
         </CardHeader>
         <CardContent>
-          <p>Sign in to your account</p>
+          <p>or</p>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className={`my-8 text-left`}>
+              <FormField name="email" control={form.control} render={(field) => (
+                <FormItem className={`my-4`}>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="person@example.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}/>
+              <FormField name="password" control={form.control} render={(field) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}/>
+              <Button className={`mt-8 w-full`} type="submit" variant="brand">
+                { isPending && (
+                  <ThreeDots color="white" height={24} width={24} />
+                )}
+                {!isPending && 'Sign In'}
+              </Button>
+            </form>
+          </Form>
         </CardContent>
         <CardFooter>
           <p>{`Don't have an account? `}<a href="/signup" className={`text-sky-950`}>Sign Up</a></p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,25 +11,21 @@ export default function Home() {
         <div className={`my-12 flex flex-row items-center w-full`}>
           <div className={`mx-12 flex flex-col items-center h-72 bg-sky-500 w-1/2 text-white p-4 py-8`}>
             <h3 className={`font-bold text-xl`}>Performers</h3>
-            <p>
               <ul className={`mt-8 text-left flex flex-col gap-4`}>
                 <li>- Audition for projects, apply for rosters</li>
                 <li>- Easy reference to pay rates, AI protections, and more</li> 
                 <li>- Host demos and embed them in your website</li>
                 <li>- Prove your identity via cross-reference verification</li>
               </ul>
-            </p>
           </div>
           <div className={`mx-12 flex flex-col items-center h-72 bg-emerald-500 w-1/2 text-white p-4 py-8`}>
             <h3 className={`font-bold text-xl`}>Project Managers/Directors</h3>
-            <p>
               <ul className={`mt-8 text-left flex flex-col gap-4`}>
                 <li>- Create standardized casting calls, attract professional talent</li>
                 <li>- Ensure auditions are properly formatted and named</li>
                 <li>- Create and manage talent rosters for future projects</li>
                 <li>- Integrate with existing tools in your workflow</li>
               </ul>
-            </p>
           </div>
         </div>
         <div>
