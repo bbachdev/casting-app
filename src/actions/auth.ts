9952da398b5f248be87a4e4e17aa2231d48edce4
Barry Bach <bbach7139@gmail.com>
2024-08-07 20:53:26 -0500

    Add sign out function

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -7,6 +7,7 @@ import { lucia } from '@/utils/auth';
 import { cache } from 'react';
 import { Session, User } from 'lucia';
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 type ServerActionResponse = {
   status: number;
@@ -79,3 +80,20 @@ export const validateRequest = cache(
 		return result;
 	}
 );
+
+export async function signOut() {
+  const { session } = await validateRequest();
+  if(!session) {
+    return {
+      status: 401,
+      body: 'Unauthorized.'
+    }
+  }
+  await lucia.invalidateSession(session.id);
+  const sessionCookie = lucia.createBlankSessionCookie();
+  cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+  return {
+    status: 200,
+    body: 'Signed out successfully.'
+  }
+}
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,23 +1,26 @@
 'use client'
-import { validateRequest } from '@/actions/auth';
+import { validateRequest, signOut } from '@/actions/auth';
 import { Button } from '../ui/button';
 import { useTheme } from "next-themes"
 import Link from 'next/link';
 import { MdLightMode } from "react-icons/md";
 import { MdDarkMode } from "react-icons/md";
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 
 export default function Header() {
+  const [hasLoaded, setHasLoaded] = useState(false)
   const [user, setUser] = useState<any | null>(null)
   const { resolvedTheme, setTheme } = useTheme()
+  const router = useRouter()
 
   const checkUserValidation = async () => {
     const { user } = await validateRequest()
     if(user) {
       setUser(user)
     }
-    console.log(user)
+    setHasLoaded(true)
   }
 
   useEffect(() => {
@@ -28,34 +31,45 @@ export default function Header() {
     setTheme((resolvedTheme === "dark" ? "light" : "dark"))
   }
 
+  async function attemptSignOut() {
+    const response = await signOut()
+    if(response.status === 200) {
+      console.log('Successful logout')
+      setUser(null)
+      router.push('/')
+    }else{
+      console.log('Unsuccessful logout: ', response.body)
+    }
+  }
+
   return (
     <header className={`w-full flex py-4 px-6 bg-sky-900 shadow-md`}>
-      <div className={`flex max-w-[1440px] w-full mx-auto`}>
-        <ul className={`flex my-auto text-white`}>
-          <Link href="/">Home</Link>
-        </ul>
-        <ul className={`ml-auto flex my-auto gap-4`}>
-          { !user && (
-            <Link href="/signin">
-              <Button variant="brand">
-                Sign In
-              </Button>
-            </Link>   
-          )}
-          { user && (
-            <Link href="/signin">
-              <Button variant="brand">
-                Sign Out
-              </Button>
-            </Link>
-          )}
-          <Button variant="brand" onClick={() => toggleTheme()}>
-            {resolvedTheme === "dark" ? <MdLightMode /> : <MdDarkMode />}
-          </Button>
-        </ul>
-      </div>
-      
-      
+      <Suspense>
+          <div className={`flex max-w-[1440px] w-full mx-auto`}>
+          <ul className={`flex my-auto text-white`}>
+            <Link href="/">Home</Link>
+          </ul>
+          <ul className={`ml-auto flex my-auto gap-4`}>
+            { !user && hasLoaded && (
+              <Link href="/signin">
+                <Button variant="brand">
+                  Sign In
+                </Button>
+              </Link>   
+            )}
+            { user && hasLoaded && (
+              <Link href="/signin">
+                <Button variant="brand" onClick={() => attemptSignOut()}>
+                  Sign Out
+                </Button>
+              </Link>
+            )}
+            <Button variant="brand" onClick={() => toggleTheme()}>
+              {resolvedTheme === "dark" ? <MdLightMode /> : <MdDarkMode />}
+            </Button>
+          </ul>
+        </div>
+      </Suspense>
     </header>
   )
 }
